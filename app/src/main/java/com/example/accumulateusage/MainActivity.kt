8b7d55bc9dc6895af8681e86b8b8b060e0d4ac63
M0710Fa <b1018188.fun@gmail.com>
2022-09-23 00:23:10 +0900

    create permission request fun

--- app/src/main/java/com/example/accumulateusage/MainActivity.kt
+++ app/src/main/java/com/example/accumulateusage/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.accumulateusage
 
+import android.app.AppOpsManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Process
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +17,10 @@ import com.example.accumulateusage.ui.theme.AccumulateUsageTheme
 import com.example.accumulateusage.ui.theme.MainScreen
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
+    private var isUsageStatsPermissionGranted = checkUsageStatsPermission()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,4 +35,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun requestPermission(){
+        if(checkUsageStatsPermission()){
+            Log.i(TAG,"UsageStats Permission is Granted")
+        }else{
+            Log.i(TAG,"UsageStats Permission is NOT Granted")
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+
+        }
+    }
+
+    private fun checkUsageStatsPermission():Boolean{
+
+        val appOpsManager = getSystemService(APP_OPS_SERVICE) as AppOpsManager
+        val mode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            appOpsManager.unsafeCheckOpNoThrow("android:get_usage_stats", Process.myUid(), packageName)
+        }else {
+            appOpsManager.checkOpNoThrow("android:get_usage_stats", Process.myUid(), packageName)
+        }
+        return mode == AppOpsManager.MODE_ALLOWED
+    }
+
 }
