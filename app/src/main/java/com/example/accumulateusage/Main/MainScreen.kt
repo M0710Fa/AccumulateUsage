5fd91c03da3a33dbfba4c03d2911d3e123cf3216
M0710Fa <b1018188.fun@gmail.com>
2022-10-09 17:35:17 +0900

    fix ui

--- app/src/main/java/com/example/accumulateusage/Main/MainScreen.kt
+++ app/src/main/java/com/example/accumulateusage/Main/MainScreen.kt
@@ -1,13 +1,17 @@
 package com.example.accumulateusage.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.accumulateusage.GetUsageStats
 import com.example.accumulateusage.Main.MainViewModel
@@ -18,24 +22,33 @@ fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
-    val scroll = rememberScrollState(0)
-
     val usageList by viewModel.usageList.collectAsState()
 
-    Column() {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
         Text(
             text = "スマートフォンの使用履歴を記憶します",
+            modifier = modifier.align(Alignment.CenterHorizontally).padding(8.dp)
         )
-        Button(onClick = {
-            viewModel.saveUsageStats(usageStats)
-        }) {
+
+        Button(
+            onClick = {
+                viewModel.saveUsageStats(usageStats)
+            },
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text(text = "GetUsage")
         }
-        Button(onClick = {
-            viewModel.readUsage()
-        }) {
+        Button(
+            onClick = {
+                viewModel.readUsage()
+            },
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text(text = "ReadUsage")
         }
+
         LazyColumn {
             items(usageList){
                 Text(text = it)
