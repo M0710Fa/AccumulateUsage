4f10fa2ddf58e231840eb9e9e4fc7033c1c44580
M0710Fa <b1018188.fun@gmail.com>
2022-10-23 02:19:05 +0900

    set permissionDemand transition

--- app/src/main/java/com/example/accumulateusage/Main/MainScreen.kt
+++ app/src/main/java/com/example/accumulateusage/Main/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.example.accumulateusage.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,46 +16,53 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.accumulateusage.AccumulateUsageApp
 import com.example.accumulateusage.GetUsageStats
 import com.example.accumulateusage.Main.MainViewModel
 
 @Composable
 fun MainScreen(
-    usageStats: GetUsageStats,
     viewModel: MainViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
     val usageList by viewModel.usageList.collectAsState()
 
-    Column(
-        modifier = modifier.padding(16.dp)
+    Surface(
+        modifier = modifier.fillMaxHeight().fillMaxWidth()
     ) {
-        Text(
-            text = "スマートフォンの使用履歴を記憶します",
-            modifier = modifier.align(Alignment.CenterHorizontally).padding(8.dp)
-        )
-
-        Button(
-            onClick = {
-                viewModel.saveUsageStats(usageStats)
-            },
-            modifier = modifier.align(Alignment.CenterHorizontally)
+        Column(
+            modifier = modifier.padding(16.dp)
         ) {
-            Text(text = "GetUsage")
-        }
-        Button(
-            onClick = {
-                viewModel.readUsage()
-            },
-            modifier = modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "ReadUsage")
-        }
+            Text(
+                text = "スマートフォンの使用履歴を記憶します",
+                modifier = modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(8.dp)
+            )
 
-        LazyColumn {
-            items(usageList){
-                Text(text = it)
+            Button(
+                onClick = {
+                    viewModel.saveUsageStats(GetUsageStats(context = AccumulateUsageApp.instance))
+                },
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "GetUsage")
+            }
+            Button(
+                onClick = {
+                    viewModel.readUsage()
+                },
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "ReadUsage")
+            }
+
+            LazyColumn {
+                items(usageList){
+                    Text(text = it)
+                }
             }
         }
     }
+
 }
