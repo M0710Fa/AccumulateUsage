d15cf83af73c7607548b8d588a4690e8ad14f6c2
M0710Fa <b1018188.fun@gmail.com>
2023-01-12 15:45:46 +0900

    [feat]フォルダ名を変更

--- app/src/main/java/com/example/accumulateusage/model/repository/UsageRepositoryImpl.kt
+++ app/src/main/java/com/example/accumulateusage/model/repository/UsageRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.example.accumulateusage.model.repository
 
 import android.app.usage.UsageStats
-import com.example.accumulateusage.model.source.local.UsageFileDataSource
+import com.example.accumulateusage.model.source.localfiles.UsageFileDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
