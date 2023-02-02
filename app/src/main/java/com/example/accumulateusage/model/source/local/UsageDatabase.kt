4407a23910b920c139986f6d04461c92d9936241
M0710Fa <b1018188.fun@gmail.com>
2023-02-02 16:18:49 +0900

    [feat]Date型を扱えるようにコンバーターを作成

--- app/src/main/java/com/example/accumulateusage/model/source/local/UsageDatabase.kt
+++ app/src/main/java/com/example/accumulateusage/model/source/local/UsageDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Usage::class], version = 0)
+@TypeConverters(DateConverters::class)
 abstract class UsageDatabase: RoomDatabase() {
     abstract fun usageDao(): UsageDao
 
