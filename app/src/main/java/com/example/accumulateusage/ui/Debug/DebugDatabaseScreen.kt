93d5a36d1b7d9dcbbfac134efe477f41e879eb57
M0710Fa <b1018188.fun@gmail.com>
2023-02-16 01:44:46 +0900

    デバッグ画面のui表示の調整

--- app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
+++ app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
@@ -1,11 +1,16 @@
 package com.example.accumulateusage.ui.Debug
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun DebugDatabaseScreen() {
-    LazyColumn() {
+    Column() {
+        Text(text = "Roomデータベース")
+        LazyColumn() {
 
+        }
     }
 }
--- app/src/main/java/com/example/accumulateusage/ui/Debug/DebugScreen.kt
+++ app/src/main/java/com/example/accumulateusage/ui/Debug/DebugScreen.kt
@@ -1,12 +1,16 @@
 package com.example.accumulateusage.ui.Debug
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
 fun DebugScreen() {
-    TopAppBar() {
-        Text(text = "デバッグスクリーン")
+    Column() {
+        TopAppBar(
+            title = { Text(text = "デバッグスクリーン") }
+        )
+        DebugDatabaseScreen()
     }
 }
