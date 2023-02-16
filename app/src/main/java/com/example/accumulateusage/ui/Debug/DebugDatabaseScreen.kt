7503f89fbbaf6ffe074a2e53218632ef7bc02de2
M0710Fa <b1018188.fun@gmail.com>
2023-02-16 17:34:29 +0900

    挿入したデータを表示できるようにした．

--- app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
+++ app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
@@ -2,9 +2,12 @@ package com.example.accumulateusage.ui.Debug
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.accumulateusage.model.source.local.Usage
 import java.util.*
@@ -13,6 +16,9 @@ import java.util.*
 fun DebugDatabaseScreen(
     viewModel: DebugDatabaseViewModel = hiltViewModel()
 ) {
+
+    val usages by viewModel.usages.collectAsState()
+
     Column() {
         Text(text = "Roomデータベース")
 
@@ -25,8 +31,16 @@ fun DebugDatabaseScreen(
             Text(text = "モックデータの追加")
         }
 
-        LazyColumn() {
+        Button(
+            onClick = { viewModel.getUsages() }
+        ) {
+            Text(text = "データ表示")
+        }
 
+        LazyColumn {
+            items(usages){
+                Text(text = it.toString())
+            }
         }
     }
 }
