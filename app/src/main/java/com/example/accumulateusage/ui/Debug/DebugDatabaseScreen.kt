19aa68ec2e6c27fb5f120447d1ce4c0caca3e863
M0710Fa <b1018188.fun@gmail.com>
2023-02-16 17:10:22 +0900

    roomにデータの挿入を可能にした

--- app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
+++ app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseScreen.kt
@@ -2,13 +2,29 @@ package com.example.accumulateusage.ui.Debug
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.accumulateusage.model.source.local.Usage
+import java.util.*
 
 @Composable
-fun DebugDatabaseScreen() {
+fun DebugDatabaseScreen(
+    viewModel: DebugDatabaseViewModel = hiltViewModel()
+) {
     Column() {
         Text(text = "Roomデータベース")
+
+        Button(
+            onClick = {
+                val usage = Usage(id = 0, name = "usage", date = Date(System.currentTimeMillis()), time = 100)
+                viewModel.addUsage(usage)
+            }
+        ) {
+            Text(text = "モックデータの追加")
+        }
+
         LazyColumn() {
 
         }
--- app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseViewModel.kt
+++ app/src/main/java/com/example/accumulateusage/ui/Debug/DebugDatabaseViewModel.kt
@@ -1,9 +1,24 @@
 package com.example.accumulateusage.ui.Debug
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.accumulateusage.model.repository.UsageRepository
+import com.example.accumulateusage.model.source.local.Usage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class DebugDatabaseViewModel @Inject constructor(
-
-){
-
+    private val usageRepository: UsageRepository
+): ViewModel(){
+    fun addUsage(usage: Usage) = viewModelScope.launch {
+        try {
+            usageRepository.addUsage(usage)
+            Log.d("debug", "Inserted")
+        }catch (e: Exception){
+            Log.d("debug", "Insert Error: $e")
+        }
+    }
 }
